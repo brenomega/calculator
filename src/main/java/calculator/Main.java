bf204d54595328c1e5d9d4d4585f37dd1f368707
PedroAlmeida08 <jp_almeida@id.uff.br>
2025-09-23 22:07:48 -0300

    feat: reading from files

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,19 +1,25 @@
 package calculator;
 
 import java.io.StringReader;
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import exception.ParserException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String expression = "3**5/-(-3)";
-		
+		String filePath = "cases/entrada3.txt";
+
 		try {
+			String expression = Files.readString(Paths.get(filePath));
+			expression = expression.trim();
 			Parser parser = new Parser(new Lexer(new StringReader(expression)));
 			System.out.println(parser.parse());
+
 		} catch (ParserException e) {
 			System.err.println("Erro de análise: " + e.getMessage());
+		} catch (Exception e) {
+			System.err.println("Erro: " + e.getMessage());
 		}
 	}
 }
